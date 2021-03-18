781e7d740fd4e23d66548830752084ea1b8137c7
Vlad <vlad6561@gmail.com>
2021-03-18 14:20:59 +0200

    Before starting DIP refactoring

---
+++ Week3/src/ro/ase/csie/cts/g1093/Laboratory3/stage3/ProductType.java
@@ -0,0 +1,17 @@
+package ro.ase.csie.cts.g1093.Laboratory3.stage3;
+
+public enum ProductType {
+    NEW(0), DISCOUNT(0.1f), LIMITED_STOCK(0.25f), LEGACY(0.35f);
+	
+	private final float discountValue;
+	
+	private ProductType(float discount)
+	{
+		this.discountValue = discount;
+	}
+
+	public float getDiscount() {
+		return this.discountValue;
+	}
+	
+}
