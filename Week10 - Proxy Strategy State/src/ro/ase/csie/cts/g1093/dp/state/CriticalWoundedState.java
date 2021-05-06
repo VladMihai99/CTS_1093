112ebe019fa6c3a867661fecc1bd84576161625d
VladMihai99 <vlad6561@gmail.com>
2021-05-06 15:40:59 +0300

    Strategy and Proxy implementation

---
+++ Week10 - Proxy Strategy State/src/ro/ase/csie/cts/g1093/dp/state/CriticalWoundedState.java
@@ -0,0 +1,12 @@
+package ro.ase.csie.cts.g1093.dp.state;
+
+public class CriticalWoundedState implements MoveStateInterface{
+
+	@Override
+	public void moveAtDifferentLocation() {
+		System.out.println("The hero cannot move!");
+		
+	}
+	
+
+}
