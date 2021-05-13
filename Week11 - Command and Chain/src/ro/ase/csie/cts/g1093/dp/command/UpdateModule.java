e077b8a0b540b7b94b42ec836a6ebd85119e2a05
VladMihai99 <vlad6561@gmail.com>
2021-05-13 15:26:18 +0300

    Implementing Command

---
+++ Week11 - Command and Chain/src/ro/ase/csie/cts/g1093/dp/command/UpdateModule.java
@@ -0,0 +1,10 @@
+package ro.ase.csie.cts.g1093.dp.command;
+
+public class UpdateModule implements GameModuleInterface {
+
+	public void doTheTask(String details) {
+		System.out.println("Updating for " + details);
+		
+	}
+
+}
